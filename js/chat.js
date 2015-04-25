76a961de52892437d06e3fc523f593d90fa8224e
esanchez47git <esanchez47@gmail.com>
2015-04-24 22:10:12 -0300

    ultimo avance

--- js/chat.js
+++ js/chat.js
@@ -9,13 +9,42 @@ var mensaje = new Chat('Usuario', 'Mensaje');
 
 Chat.mensaje = []; // array de mensajes
 
+// send mensaje
 Chat.enviar = function(e){
 	e.preventDefault();
 	var usuario = document.getElementById('usuario').value;
 	var mensaje = document.getElementById('chatmsg').value;
 	var msg = new Chat(usuario, chatmsg);
-	
 	Chat.mensaje.push(msg);
-
-	Chat.enviar(e);	
+	console.log(Chat.mensaje);
+	
+	Chat.borrar(e);
+	Chat.mostrar();
 }
+
+// clear mensaje
+Chat.borrar = function(e){
+	var mensaje = document.getElementById('chatmsg');
+	chatmsg.value ='';
+
+}
+
+// mostrar chats
+Chat.mostrar = function(){
+	var conversa = '';
+	Chat.mensaje.map(function(elemento, indice){
+
+		conversa += '<article>';
+				 + '<h4>' + elemento.usuario + '</h4>';
+				 + '<p>' + elemento.chatmsg + '</p>';
+				 + '</article>';
+	})
+
+	document.getElementById('conversa').innerHTML = conversa;
+}
+
+
+
+
+
+
